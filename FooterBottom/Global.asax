7fba015a2c5e754648980b25725251a161f5d2fa
winifred <wcadap@gmail.com>
2014-05-14 19:35:13 +0800

    First commit

---
+++ FooterBottom/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FooterBottom.MvcApplication" Language="C#" %>
---
+++ FooterBottom/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FooterBottom.Startup))]
+namespace FooterBottom
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
